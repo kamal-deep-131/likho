afb4e4e571a3122b34adbc83d549e4a394626ac9
kamal-deep-131 <kamaldeepsinghsodhi2@gmail.com>
2024-08-03 08:39:50 +0530

    enviromental variables configuration

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
 import express from "express";
 import blogRoutes from './routes/blog.routes.js'
+import dotenv from 'dotenv';
 
+dotenv.config()
+console.log(process.env.TEST)
 const app = express()
 const PORT = 3000 || process.env.PORT
 
